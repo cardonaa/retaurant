96c30db60edc13d6f6a0144632d6e40794d95567
cardonaa <cardonaiush@hotmail.com>
2013-09-19 15:42:03 -0500

    19/09

--- server/ws.js
+++ server/ws.js
@@ -3,38 +3,56 @@ module.exports = function (servidor, cache){
   
   var sio = require('socket.io');
   var ws = sio.listen(servidor);
+  var mongoose = require('mongoose');
+
+  mongoose.connect('mongodb://localhost/ptos');
+
+  var ptosSchema = mongoose.Schema({
+    tipo: String ,
+    pto : String ,
+    precio : Number 
+  });
+
+  var pto = mongoose.model('ptos', ptosSchema);
+  
+   pto.find().exec(function(err, doc){
+     console.log(err);
+     console.log(doc);
+   });
+   
+  /*/ CREANDO UN PTO PARA INSERTAR.
+  var primerpto = new pto({
+    tipo : 'Refrescos',
+    pto : '7up',
+    precio: 1500
+  });
+  /*/
+  
+
+   /*/ SAVES  
+  primerpto.save(function(err, doc){
+     if(err)
+        console.log(err);
+  });
+   /*/
+
+    /*/ EVENTO CONNECTION
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function callback () {
+      console.log("Bd coenctada.");
+    });
+    /*/
+
+  /*/ =================================================================================/*/
 
   ws.disable('log'); 
   // websocket, htmlfile, xhr-polling, jsonp-polling
   ws.on('connection', function (socket){
 
     console.log("la id es",socket.id);
-
     socket.emit('ready',{title: 'Ready'});
 
-    // emito los ptos.
-    /*/var ptos = {};
-
-    ptos[0].tipo = "Sopas:";
-    ptos[0].nombre = "Sopa de pollo.";
-    ptos[0].precio = 3500;
-    
-    ptos[0].tipo = "Sopas:";
-    ptos[0].nombre = "Sopa de pollo.";
-    ptos[0].precio = 3500;
-    
-    ptos[0].tipo = "Sopas:";
-    ptos[0].nombre = "Sopa de pollo.";
-    ptos[0].precio = 3500;
-    
-    ptos[0].tipo = "Sopas:";
-    ptos[0].nombre = "Sopa de pollo.";
-    ptos[0].precio = 3500;
-    
-    ptos[0].tipo = "Sopas:";
-    ptos[0].nombre = "Sopa de pollo.";
-    ptos[0].precio = 3500;
-    /*/
 
     socket.on('imagen', function (imagen){
       cache[imagen.id] = imagen.data;
